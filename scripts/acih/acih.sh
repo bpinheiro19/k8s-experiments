737609d7a677827f4dba1d545e89f9fd30d2de33
bpinheiro <bernardopinheiro19@gmail.com>
2025-09-29 22:18:07 +0100

    implemented ACI with NatGateway

--- scripts/acih/acih.sh
+++ scripts/acih/acih.sh
@@ -8,6 +8,8 @@ vnet="aci-vnet$rand"
 vnetAddr=10.0.0.0/16
 subnet="aci-subnet"
 subnetAddr=10.0.240.0/24
+publicIpName="myPublicIP"
+natGatewayName="myNATgateway"
 
 #ACI
 aci="appcontainer$rand"
@@ -22,6 +24,7 @@ aci() {
     echo "## 01 - Azure Container Instance with Public IP               ##"
     echo "## 02 - Azure Container instance with Azure CLI image         ##"
     echo "## 03 - Azure Container instance with vnet                    ##"
+    echo "## 04 - Azure Container instance with NAT Gateway             ##"
     echo "################################################################"
 
     while read -p "Option: " opt; do
@@ -37,6 +40,10 @@ aci() {
             ;;
         3)
             createVnetACI
+            break
+            ;;
+        4)
+            createACINatGateway
             break
             ;;
         esac
@@ -63,12 +70,6 @@ createACI() {
     echo "Creating Azure Container Instance"
     az container create --name $aci --resource-group $rg --image $image --cpu $cpu --memory $mem --os-type $osType $1
 }
-createACIWithRGAndVNET() {
-    createRG
-    createVNET
-
-    createACI $1
-}
 
 createPublicACI() {
     createRG
@@ -86,6 +87,26 @@ createACIAzureCLI() {
     createRG
     echo "Creating Azure Container Instance with Azure CLI image"
     az container create --name $aci --resource-group $rg --image "mcr.microsoft.com/azure-cli" --cpu $cpu --memory $mem --os-type $osType --command-line 'tail -f /dev/null'
+}
+
+createNatGateway(){
+    echo "Creating Public IP for NAT Gateway"
+    az network public-ip create --name $publicIpName --resource-group $rg  --sku standard --zone 1 --allocation static
+   
+    echo "Creating NAT Gateway"
+    az network nat gateway create --resource-group $rg --name $natGatewayName --public-ip-addresses $publicIpName --idle-timeout 10
+
+    az network vnet subnet update --resource-group $rg --vnet-name $vnet --name $subnet --nat-gateway $natGatewayName
+}
+
+createACINatGateway() {
+    echo "Creating Azure Container Instance with Nat Gateway"
+    createRG
+    createVNET
+    
+    createNatGateway
+
+    createACI "--vnet $vnet --subnet $subnet"
 }
 
 header() {

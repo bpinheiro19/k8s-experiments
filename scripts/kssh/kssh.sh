ec751a5bad64859396cd1dcb5e878ba30d983854
bpinheiro <bernardopinheiro19@gmail.com>
2025-11-15 17:31:14 +0000

    Restructured kssh script

--- scripts/kssh/kssh.sh
+++ scripts/kssh/kssh.sh
@@ -2,15 +2,21 @@
 
 mapfile -t nodes < <(kubectl get nodes | tail -n +2 | awk '{print $1}')
 
-for i in "${!nodes[@]}"; do
-  printf "$i ${nodes[i]} \n"
-done
-        
-read -p "Enter the node number: " NODE_INDEX
-NODE=${nodes[NODE_INDEX]}
+size=${#nodes[@]}
 
-IMAGE="mcr.microsoft.com/dotnet/runtime-deps:6.0"
-PROFILE="netadmin"
-ARGS="nsenter --target 1 --mount --uts --ipc --net --pid -- bash -l"
+if [ $size -eq 0 ]; then
+    echo "No Nodes available"
+else
+  for i in $(seq 1 $size); do
+    printf "$(($i)) - ${nodes[i-1]} \n"
+  done
 
-kubectl debug node/$NODE -it  --image=$IMAGE --profile=$PROFILE -- $ARGS
+  read -p "Enter the node number: " index
+
+  if [ -z $index ] || ( [[ $index =~ ^[1-9]+$ ]] && (( $index <= size ))); then
+    NODE=${nodes[index-1]}
+    kubectl debug node/$NODE -it --image=mcr.microsoft.com/azurelinux/busybox:1.36 --profile=sysadmin -- sh -xc "chroot /host"
+  else
+    echo "$index is not a valid index."
+  fi 
+fi
